8b8f9f61a2969dc654b660787a979794d32a72d2
Froztbyt3 <Jul_daniel@hotmail.com>
2021-01-25 18:24:12 +0100

    Added adapt intents

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_handler
+from adapt.intent import IntentBuilder
 from google_calendar import getEventData
 from OpenWeather_api_client import weatherClient
 
@@ -7,7 +8,11 @@ class GoogleCalendarReaderSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler('reader.calendar.google.intent')
+    @intent_handler(IntentBuilder('reader.calendar.google.intent')
+                    .require('Calendar')
+                    .require('What')
+                    .require('My')
+                    .require('Is'))
     def handle_reader_calendar_google(self, message):
 
         self.speak_dialog('meeting', data=self.getEventData())
@@ -20,5 +25,5 @@ def create_skill():
     return GoogleCalendarReaderSkill()
 
 
-if __name__ == '__main__':
-    create_skill()
+# if __name__ == '__main__':
+#     create_skill()
